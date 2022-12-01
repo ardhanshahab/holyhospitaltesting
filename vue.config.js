29aa23cf498ce9be1caad47c52344749100924ca
Muhamad Ardhan Hadaina <65588203+ardhanshahab@users.noreply.github.com>
2022-12-01 15:03:54 +0700

    fix test v2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
+const { VueLoaderPlugin } = require('vue-loader')
 module.exports = defineConfig({
   devServer: {
     proxy: 'https://go-hospital-server.herokuapp.com'},
   transpileDependencies: true,
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
 })
+
